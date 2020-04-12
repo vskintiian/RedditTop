14cc6d7126aac5e89861224e583962765ee8ec10
Vladyslav Skintiyan <vskintiyan@alexs-MacBook-Pro.local>
2020-04-12 13:22:25 +0300

    project created

---
+++ RedditTop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditTop
+//
+//  Created by Vladyslav Skintiyan on 12.04.2020.
+//  Copyright © 2020 Vladyslav Skintiyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
